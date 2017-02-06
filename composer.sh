fb9c152a83a631387e5532686338eab78b22cb55
Daniel Upshaw <contact@danielupshaw.com>
2017-02-06 18:58:29 -0500

    Create composer.sh

---
+++ composer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+github_url="$1"
+
+echo ">>> Installing Composer"
+cd ~
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+sudo mv composer.phar /usr/sbin/composer
