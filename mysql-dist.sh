e950b9d88d81a193a9f729bc9cc7e7bed7ead667
Daniel Upshaw <contact@danielupshaw.com>
2017-02-06 18:50:18 -0500

    Update mysql-dist.sh

--- mysql-dist.sh
+++ mysql-dist.sh
@@ -4,9 +4,14 @@ github_url="$1"
 mysql_root_password="$2"
 mysql_create_database="$3"
 
+echo ">>> Installing MySQL latest distribution version"
+
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password ${mysql_root_password}"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${mysql_root_password}"
 
-sudo apt-get install mysql-server
+sudo apt-get install -qq mysql-server
 
-[[ -n "${mysql_create_database}" ]] && echo "CREATE DATABSE IF NOT EXISTS ${mysql_create_database} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql -u root username -p"${mysql_root_password}"
+if [[ -n "${mysql_create_database}" ]]; then
+	echo ">>> Creating new MySQL database"
+	echo "CREATE DATABSE IF NOT EXISTS ${mysql_create_database} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql -u root username -p"${mysql_root_password}"
+fi
