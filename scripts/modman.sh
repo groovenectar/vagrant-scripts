02360680f31dd9c34e68cb03130410425a76ae57
Daniel Upshaw <contact@danielupshaw.com>
2017-02-06 19:30:13 -0500

    Update modman.sh

--- scripts/modman.sh
+++ scripts/modman.sh
@@ -6,10 +6,10 @@ SRC="https://raw.githubusercontent.com/colinmollenhour/modman/master/modman"
 DEST="/usr/sbin/modman"
 
 # test if curl/wget is installed
-if hash curl 2>&- ; then
-    CMD="sudo curl -s -L $SRC -o $DEST"
-elif hash wget 2>&- ; then
+if hash wget 2>&- ; then
     CMD="sudo wget -q --no-check-certificate -O $DEST $SRC"
+elif hash curl 2>&- ; then
+    CMD="sudo curl -s -L $SRC -o $DEST"
 else
    echo "You need to have curl or wget installed."
    exit 1
