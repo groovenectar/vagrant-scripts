c9c6f13c1bb2cb18f3c2a4d942f19f9611588394
Daniel Upshaw <contact@danielupshaw.com>
2017-02-06 19:05:53 -0500

    Update nginx-dist.sh

--- nginx-dist.sh
+++ nginx-dist.sh
@@ -13,6 +13,9 @@ sudo sed -i "s/user www-data;/user vagrant;/" /etc/nginx/nginx.conf
 sudo sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 
 sudo wget -O /etc/nginx/nginx.conf ${github_url}/nginx/nginx.conf
+sudo wget -O /etc/nginx/fastcgi_params ${github_url}/nginx/fastcgi_params
+sudo wget -O /etc/nginx/fastcgi.conf ${github_url}/nginx/fastcgi_params
+
 sudo wget -O /etc/nginx/sites-available/default ${github_url}/nginx/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
