aadcf9dc298172593d8744d380d85ab0755a9f32
Daniel Upshaw <contact@danielupshaw.com>
2017-02-06 19:39:23 -0500

    Update php70-dist.sh

--- scripts/php70-dist.sh
+++ scripts/php70-dist.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo ">>> Installing PHP7.0"
-sudo apt-get install -qq php7.0
+sudo apt-get -qq install php7.0
 
 echo ">>> Configuring PHP7.0"
 sudo sed -i "s/max_execution_time = .*/max_execution_time = 180/" /etc/php/7.0/fpm/php.ini
@@ -12,7 +12,7 @@ sudo sed -i "s/max_execution_time = .*/max_execution_time = 180/" /etc/php/7.0/c
 sudo sed -i "s/memory_limit = .*/memory_limit = 1024M/" /etc/php/7.0/cli/php.ini
 
 echo ">>> Installing PHP7.0 modules"
-sudo apt-get install -qq php7.0-mcrypt
+sudo apt-get -qq install php7.0-mcrypt
 sudo phpenmod mcrypt
 
 sudo service php7.0-fpm restart
