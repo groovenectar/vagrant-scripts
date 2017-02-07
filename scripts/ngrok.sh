94b87594723109c8825d0463e14099f9575baa2a
Daniel Upshaw <contact@danielupshaw.com>
2017-02-06 19:29:12 -0500

    Update ngrok.sh

--- scripts/ngrok.sh
+++ scripts/ngrok.sh
@@ -2,7 +2,7 @@
 
 echo ">>> Installing ngrok"
 
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 dtrx --one here ngrok-stable-linux-amd64.zip
 sudo mv ngrok /usr/bin/ngrok
 rm ngrok-stable-linux-amd64.zip
