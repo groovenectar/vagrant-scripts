ff35e4059b32d7e4749f73c6a202642264df2eb6
Daniel Upshaw <contact@danielupshaw.com>
2017-02-07 12:27:34 -0500

    Update phpmyadmin.sh

--- scripts/phpmyadmin.sh
+++ scripts/phpmyadmin.sh
@@ -2,6 +2,8 @@
 
 github_url="$1"
 public_folder="$2"
+hostname="$3"
+server_ip="$4"
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -15,3 +17,13 @@ sudo mv master /var/www/phpmyadmin
 sudo ln -s /var/www/phpmyadmin ${public_folder}/phpmyadmin
 cd /var/www/phpmyadmin
 composer -q install > /dev/null
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ">>> phpMyAdmin installed at URL http://${server_ip}/phpmyadmin or http://${hostname}/phpmyadmin"
+echo ""
+echo ""
+echo ""
+echo ""
