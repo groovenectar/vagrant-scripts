9e36835fe0946c038d4564ab893461b3ecc8bb98
Daniel Upshaw <contact@danielupshaw.com>
2017-02-08 12:35:38 -0500

    Update phpunit-5.sh

--- scripts/phpunit-5.sh
+++ scripts/phpunit-5.sh
@@ -7,5 +7,5 @@ export DEBIAN_FRONTEND=noninteractive
 echo ">>> Installing PHPUnit"
 cd ~
 wget -q https://phar.phpunit.de/phpunit-5.7.phar > /dev/null
-sudo mv phpunit.phar /usr/sbin/phpunit
+sudo mv phpunit-5.7.phar /usr/sbin/phpunit
 sudo chmod +x /usr/sbin/phpunit
