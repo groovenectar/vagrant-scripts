f9f1b964a43e80d5fb6eb75518e6ff680a64a6f9
Daniel Upshaw <contact@danielupshaw.com>
2017-03-17 02:38:43 -0400

    Update composer.sh

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -7,7 +7,8 @@ export DEBIAN_FRONTEND=noninteractive
 echo ">>> Installing Composer"
 cd ~
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+# This will not be feasible without scraping the the composer download page
+# php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php > /dev/null
 php -r "unlink('composer-setup.php');"
 
