67fe69c4f39123d320ecf38d281a1005507f20d0
Daniel Upshaw <contact@danielupshaw.com>
2018-04-05 23:02:30 -0400

    Update php7.1.sh

--- scripts/php7.1.sh
+++ scripts/php7.1.sh
@@ -28,6 +28,8 @@ sudo apt-get -qq install php7.1-mysqli > /dev/null
 # sudo apt-get -qq install php7.1-imap > /dev/null
 sudo apt-get -qq install php7.1-curl > /dev/null
 sudo apt-get -qq install php7.1-tidy > /dev/null
+sudo apt-get -qq install php7.1-pdo > /dev/null
+sudo apt-get -qq install php7.1-pdo-mysql > /dev/null
 sudo apt-get -qq install php7.1-gd > /dev/null
 sudo apt-get -qq install php7.1-zip > /dev/null
 # sudo apt-get -qq install php7.1-soap > /dev/null
