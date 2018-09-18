24db33f67bd2cf40a12a8e7af48efe7d793c0196
Daniel Upshaw <contact@danielupshaw.com>
2018-09-17 21:27:44 -0400

    Update PHPUnit installer
    This repository is no longer actively maintained

--- scripts/phpunit-latest.sh
+++ scripts/phpunit-latest.sh
@@ -4,8 +4,22 @@ github_url="$1"
 
 export DEBIAN_FRONTEND=noninteractive
 
+SBINPATH="/usr/sbin/phpunit"
+PHPUNIT_BINPATH="/home/vagrant/bin/phpunit"
+
 echo ">>> Installing PHPUnit"
-cd ~
-wget -q https://phar.phpunit.de/phpunit.phar > /dev/null
-sudo mv phpunit.phar /usr/sbin/phpunit
-sudo chmod +x /usr/sbin/phpunit
+{
+	cd ~
+	wget -q https://phar.phpunit.de/phpunit.phar
+	mkdir -p "${BINPATH}"
+	sudo mv phpunit.phar "${PHPUNIT_BINPATH}"
+	sudo chmod +x "${PHPUNIT_BINPATH}"
+
+(sudo tee "${SBINPATH}" <<EOL
+#!/usr/bin/env bash
+php ${PHPUNIT_BINPATH}
+EOL
+)
+
+	sudo chmod +x "${SBINPATH}"
+} &>> "$PROVISION_LOG"
